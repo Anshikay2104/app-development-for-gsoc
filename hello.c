6a8ce26ae5278e5c253922a1634a5d75284ceb40
Anshikay2104 <119113512+Anshikay2104@users.noreply.github.com>
2023-01-19 19:15:19 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    char name[20];
+    printf("hello welcom to our store \n please enter your name :\n");
+    scanf("%s",name);
+    printf("please choose from the following :\n 1)biscuits \n2)chips \n3)namkeen \n press the corresponding number of the item :");
+    scanf("%d",&n);
+    printf("now choose from the following brands :");
+    
+
+}
